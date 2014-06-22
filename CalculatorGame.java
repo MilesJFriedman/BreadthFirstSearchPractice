f5ea3924069762a2c7337f2128e5882238b83c19
Miles Friedman <milesfriedmanfl@gmail.com>
2014-06-22 15:22:04 -0700

    Fixed breadth first search algorithm, program successfully completes 1 test case

--- CalculatorGame.java
+++ CalculatorGame.java
@@ -59,47 +59,53 @@ public class CalculatorGame {
 			//create a new node representing the first button being pressed (add 1), also add 1 
 			//to the distance of this node from the previous nodes distance.
 			QueueNode press1st = new QueueNode(next.getDistance()+1, next.getValue()+1);
+			//System.out.println("Debugging1: " + press1st.getValue());
 			//create a new node representing the second button being pressed (multiply 3), also 
 			//add 1 to the distance of this node from the previous nodes distance.
 			QueueNode press2nd = new QueueNode(next.getDistance()+1, next.getValue()*3);
+			//System.out.println("Debugging2: " + press2nd.getValue());
 			//create a new node representing the third button being pressed (divide by 2), also
 			//add 1 to the distance of this node from the previous nodes distance.
-			QueueNode press3rd = new QueueNode(next.getDistance()+1, next.getValue()+1);
+			QueueNode press3rd = new QueueNode(next.getDistance()+1, next.getValue()/2);
+			//System.out.println("Debugging3: " + press3rd.getValue());
 			
 			//for each button pressed, so long as the integer value is within the 1 billion range
 			//of the calculator, if the value is within the 100 values to be found, and has not
 			//already been found, enqueue the new node and mark it as found. If the value is
 			//greater than the 100 values to be found, merely enqueue the node.
 			if (press1st.getValue() < 1000000000 && press1st.getValue() > 0) {
-				if (press1st.getValue() <= 100 && found[press1st.getValue()] != true)
-				queue.offer(press1st);
-				found[press1st.getValue()] = true;
-				//if this node is the last that needs to be found, output the distance from the
-				//start node required to get to this point (number of button presses needed)
-				if (AllValuesFound(found) == true)
-					System.out.println(press1st.getDistance());
+				if (press1st.getValue() <= 100 && found[press1st.getValue()] != true) {
+					queue.offer(press1st);
+					found[press1st.getValue()] = true;
+					//if this node is the last that needs to be found, output the distance from the
+					//start node required to get to this point (number of button presses needed)
+					if (AllValuesFound(found) == true)
+						System.out.println(press1st.getDistance());
+				}
 			} else
 				queue.offer(press1st);
 			
 			if (press2nd.getValue() < 1000000000 && press2nd.getValue() > 0) {
-				if (press2nd.getValue() <= 100 && found[press2nd.getValue()] != true)
-				queue.offer(press2nd);
-				found[press2nd.getValue()] = true;
-				//if this node is the last that needs to be found, output the distance from the
-				//start node required to get to this point (number of button presses needed)
-				if (AllValuesFound(found) == true)
-					System.out.println(press2nd.getDistance());
+				if (press2nd.getValue() <= 100 && found[press2nd.getValue()] != true) {
+					queue.offer(press2nd);
+					found[press2nd.getValue()] = true;
+					//if this node is the last that needs to be found, output the distance from the
+					//start node required to get to this point (number of button presses needed)
+					if (AllValuesFound(found) == true)
+						System.out.println(press2nd.getDistance());
+				}	
 			} else
 				queue.offer(press2nd);
 			
 			if (press3rd.getValue() < 1000000000 && press3rd.getValue() > 0) {
-				if (press3rd.getValue() <= 100 && found[press3rd.getValue()] != true)
-				queue.offer(press3rd);
-				found[press3rd.getValue()] = true;
-				//if this node is the last that needs to be found, output the distance from the
-				//start node required to get to this point (number of button presses needed)
-				if (AllValuesFound(found) == true)
-					System.out.println(press3rd.getDistance());
+				if (press3rd.getValue() <= 100 && found[press3rd.getValue()] != true) {
+					queue.offer(press3rd);
+					found[press3rd.getValue()] = true;
+					//if this node is the last that needs to be found, output the distance from the
+					//start node required to get to this point (number of button presses needed)
+					if (AllValuesFound(found) == true)
+						System.out.println(press3rd.getDistance());
+				}
 			} else
 				queue.offer(press3rd);
 			
